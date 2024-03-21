b83f2c950337184c3145d87ff8ea681ebb3aa6ae
liwei <wli@pvetec.com>
2024-03-21 20:26:09 +0800

    add ChitChat component

---
+++ app/core/ChitChat.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import {Icon} from "@iconify/react"
+const ChitChat = () => {
+    return (
+        <>
+            <div className={'flex flex-row pl-3 pr-16 py-4 justify-start bg-white rounded-lg'}>
+                <div className={'mr-2'}>
+                    <Icon icon={'ri:wechat-fill'} width={70}/>
+                </div>
+                <div className={'flex flex-col gap-1 mt-1'}>
+                    <p className={'font-black text-2xl'}>
+                        ChitChat
+                    </p>
+                    <p className={'text-sm text-gray-500'}>You have a new message!</p>
+                </div>
+            </div>
+        </>
+    )
+}
+export default ChitChat
